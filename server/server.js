ec8a6e0719f73f7274f0dcc584ce301688f9d9d7
Oguz EROL <oguzerol91@gmail.com>
2018-07-03 00:21:37 +0300

    user's post&get

--- server/server.js
+++ server/server.js
@@ -10,12 +10,32 @@ var id = 0;
 
 app.use(morgan('dev'))
 app.use(express.static('client'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-app.get('/users', function(req, res){
-  res.json(users);
+app.get('/users', function (req, res) {
+  res.json(users);  2
+});
+
+app.post('/users', function (req, res) {
+  var user = req.body;
+  id++;
+  user.id = id + '';
+
+  users.push(user);
+  res.json(user);
+});
+
+app.get('/users/:id', function (req, res) {
+  console.log(req.params.id);
+  if (req.params.id) {
+    var user = _.find(users, { id: req.params.id });
+    console.log(user);
+    res.json(user);
+  } else {
+    res.send();
+  }
 });
 
 app.listen(3000);
