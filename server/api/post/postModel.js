7feec44eec7ec6eeb068e619cd8aa625608eda1a
Oguz EROL <oguzerol91@gmail.com>
2018-07-10 23:44:08 +0300

    add Schema's to project

---
+++ server/api/post/postModel.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var PostSchema = new Schema({
+    title: {
+        type: String,
+        required : true,
+        unique : true
+    },
+    text: {
+        type: String,
+        required : true,
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+        required: true
+    },
+    categories: [
+        {   
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'category',
+            required: true
+        }
+    ]
+        
+});
+
+module.exports = mongoose.model('post', PostSchema);
